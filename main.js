19bf9236cdffd72ac09fb373e04e11e4546d9403
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-11-18 21:10:04 +0200

    worked on the modal display and js toggle event

---
+++ main.js
@@ -0,0 +1,26 @@
+const menuCtl = document.querySelector('.fa-bars');
+const modal = document.querySelector('.modal');
+console.log(menuCtl);
+console.log(modal);
+let count = 1;
+
+//reset modal to be visible at all times
+//when 680px or greater has been reached.
+//below 680 can be toggled by the .fa-bars
+window.onresize = function() {
+    if (window.innerWidth >= 680) {
+        modal.style.visibility = 'visible';
+       // console.log(window.innerWidth);
+    } 
+}
+
+menuCtl.addEventListener("click", function () {
+    console.log(window.innerWidth);
+    if (modal.style.visibility == "hidden") {
+        modal.style.visibility = 'visible';
+    } else {
+        modal.style.visibility = 'hidden';
+    }
+    console.log("item clicked" + count);
+    count++;
+})
